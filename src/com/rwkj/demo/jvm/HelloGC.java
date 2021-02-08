aee37339bd0ca30d21632926211a267bcc317ef6
jiuxinqkl <33077182+jiuxinqkl@users.noreply.github.com>
2021-02-08 11:17:05 +0800

    Update HelloGC.java

--- src/com/rwkj/demo/jvm/HelloGC.java
+++ src/com/rwkj/demo/jvm/HelloGC.java
@@ -2,7 +2,7 @@ package com.rwkj.demo.jvm;
 
 public class HelloGC {
     public static void main(String[] args) {
-        long totalMemory = Runtime.getRuntime().totalMemory(); //返回Java虚拟机中的内存总量
+        long totalMemory = Runtime.getRuntime().totalMemory(); //返回Java虚拟机中的内存总量是多少
         long maxMemory = Runtime.getRuntime().maxMemory();   //最大内存兩
 
         System.out.println("totalMemory(-Xms)="+totalMemory+"(字节)、"+(totalMemory/(double)1024/1024)+"MB");
